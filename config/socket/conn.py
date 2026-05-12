d02c9e9227fdeb113bcb149ac72a9534933c9506
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-05-12 11:41:49 +0500

    [CHECK_POINT]

--- config/socket/conn.py
+++ config/socket/conn.py
@@ -9,65 +9,49 @@ from service.stt import audio_pcm_stream_to_text, audio_stream_to_text
 sio = socketio.AsyncServer(
     async_mode="asgi",
     cors_allowed_origins="*",
-    # logger=True,
-    # engineio_logger=True,
 )
 sio_app = socketio.ASGIApp(socketio_server=sio)
 
-audio_dir = Path("audio")
-audio_dir.mkdir(parents=True, exist_ok=True)
-
-audio_buffers = defaultdict(bytearray)
-audio_chunk_counts = defaultdict(int)
-audio_chunk_text = defaultdict(str)
+text_tokens = defaultdict(str)
 
 
 @sio.event
 async def connect(sid, environ):
     print(f"Client connected: {sid}")
-
-
-@sio.on("audio:receive")
-async def audio_receive(sid, data):
-    chunk_count = 20
-    audio_buffers[sid].extend(data)
-
-    audio_chunk_counts[sid] += 1
-    audio_chunk_text[sid] += ""
-
-    # wait for 5 chunks
-    if audio_chunk_counts[sid] < chunk_count:
-        return
-
-    raw = bytes(audio_buffers[sid])
-
-    start = datetime.now()
-    text = audio_pcm_stream_to_text(raw)
-    end = datetime.now()
-    audio_chunk_text[sid] += f"{text} "
-    print(f"Processing: {end - start}, Text: {audio_chunk_text[sid]}")
-    # reset buffer
-    audio_buffers[sid].clear()
-    audio_chunk_counts[sid] = 0
+    
+@sio.on("audio:receive:tokens")
+async def audio_receive_tokens(sid, data: str):
+    text_tokens[sid] += data
+    print(f"[token]: {data}")
+    print(f"[text]: {text_tokens[sid]}")
+    
+@sio.on("audio:received")
+async def audio_receive_tokens(sid):
+    print(f"Generating output for text: {text_tokens[sid]}")
 
 
 # @sio.on("audio:receive")
 # async def audio_receive(sid, data):
+#     chunk_count = 20
 #     audio_buffers[sid].extend(data)
-#     raw = data
-#     # raw = audio_buffers[sid]
-#     pcm = np.frombuffer(raw, dtype=np.int16)
+
+#     audio_chunk_counts[sid] += 1
+#     audio_chunk_text[sid] += ""
+
+#     # wait for 5 chunks
+#     if audio_chunk_counts[sid] < chunk_count:
+#         return
+
+#     raw = bytes(audio_buffers[sid])
+
 #     start = datetime.now()
-#     filename = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-
-#     filepath = audio_dir / f"{filename}-debug.wav"
-#     write(str(filepath), 16000, pcm)
-
-#     text = audio_pcm_stream_to_text(data)
+#     text = audio_pcm_stream_to_text(raw)
 #     end = datetime.now()
-#     print(f"Transcribed text: {text}")
-#     print(f"Processing time: {end - start}ms")
-#     # await sio.emit("audio:receive:all:response", {"reply": f"{text}"}, to=sid)
+#     audio_chunk_text[sid] += f"{text} "
+#     print(f"Processing: {end - start}, Text: {audio_chunk_text[sid]}")
+#     # reset buffer
+#     audio_buffers[sid].clear()
+#     audio_chunk_counts[sid] = 0
 
 
 @sio.on("audio:receive:all")
