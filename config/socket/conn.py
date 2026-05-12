adbd971da531b4c36504f28a5517d34f38d397ab
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-05-12 12:57:07 +0500

    updates

--- config/socket/conn.py
+++ config/socket/conn.py
@@ -1,10 +1,5 @@
 from collections import defaultdict
-from datetime import datetime
-from fileinput import filename
-from pathlib import Path
-from scipy.io.wavfile import write
 import socketio
-from service.stt import audio_pcm_stream_to_text, audio_stream_to_text
 
 sio = socketio.AsyncServer(
     async_mode="asgi",
@@ -16,7 +11,7 @@ text_tokens = defaultdict(str)
 
 
 @sio.event
-async def connect(sid, environ):
+async def connect(sid,_):
     print(f"Client connected: {sid}")
     
 @sio.on("audio:receive:tokens")
@@ -28,37 +23,6 @@ async def audio_receive_tokens(sid, data: str):
 @sio.on("audio:received")
 async def audio_receive_tokens(sid):
     print(f"Generating output for text: {text_tokens[sid]}")
+    await sio.emit("audio:generate", text_tokens[sid])
+    text_tokens[sid] = ""
 
-
-# @sio.on("audio:receive")
-# async def audio_receive(sid, data):
-#     chunk_count = 20
-#     audio_buffers[sid].extend(data)
-
-#     audio_chunk_counts[sid] += 1
-#     audio_chunk_text[sid] += ""
-
-#     # wait for 5 chunks
-#     if audio_chunk_counts[sid] < chunk_count:
-#         return
-
-#     raw = bytes(audio_buffers[sid])
-
-#     start = datetime.now()
-#     text = audio_pcm_stream_to_text(raw)
-#     end = datetime.now()
-#     audio_chunk_text[sid] += f"{text} "
-#     print(f"Processing: {end - start}, Text: {audio_chunk_text[sid]}")
-#     # reset buffer
-#     audio_buffers[sid].clear()
-#     audio_chunk_counts[sid] = 0
-
-
-@sio.on("audio:receive:all")
-async def audio_receive_all(sid, data: bytes):
-    start = datetime.now()
-    text = audio_stream_to_text(data)
-    end = datetime.now()
-    print(f"Transcribed text: {text}")
-    print(f"Processing time: {end - start}ms")
-    await sio.emit("audio:receive:all:response", {"reply": f"{text}"}, to=sid)
