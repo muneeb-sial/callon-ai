8b9075fb28a5b1db3ad83645212e178d6dc31130
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-05-12 15:13:12 +0500

    updates

--- config/socket/audio_received.py
+++ config/socket/audio_received.py
@@ -5,6 +5,7 @@ agent = AgentService()
 
 @sio.on("audio:received")
 async def audio_received(sid):
-    response = await agent.run(text_tokens[sid])
-    await sio.emit("audio:generate", response, to=sid)
+    # response = await agent.run(text_tokens[sid])
+    await sio.emit("audio:generate", text_tokens[sid], to=sid)
+    # await sio.emit("audio:generate", response, to=sid)
     text_tokens[sid] = ""
